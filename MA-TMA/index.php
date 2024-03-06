62fbc9d1914e103c8436a92b7c7f6c0eafebddb8
Zimacq <147037243+Zimacq@users.noreply.github.com>
2024-03-06 11:21:37 +0100

    MA-TMA Uploder peer-learning v1

---
+++ MA-TMA/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="utf-8">
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="container">
+        <div class="profile-section">
+            <img src="image/michelle.png" alt="Photo de Michelle Télo" class="profile-photo">
+            <h2>Télo Thomas</h2>
+            <a href="upload_form.php" class="upload-button">Uploader vos fichiers ici</a>
+        </div>
+    </div>
+</body>
+</html>
